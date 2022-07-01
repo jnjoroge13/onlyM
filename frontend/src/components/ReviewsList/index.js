4130a13ccc84de8f8e34e5d6a22c1c321f052f92
Jesse <jnjoroge13@gmail.com>
2022-06-30 22:05:44 -0700

    review list

--- frontend/src/components/ReviewsList/index.js
+++ frontend/src/components/ReviewsList/index.js
@@ -1,9 +1,27 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux'
-import { NavLink, useHistory } from 'react-router-dom';
-
+import { NavLink, useHistory, useParams } from 'react-router-dom';
+import reviews, { thunkGetAllReviews } from '../../store/reviews';
 export default function ReviewList() {
+    const dispatch = useDispatch()
+    const { spotId } = useParams()
+    const selectorReviews = useSelector(state => Object.values(state.reviews))
+    const spotReviews = selectorReviews.filter(review => {
+        return(review.spotId == spotId)
+    })
+    const sessionUser = useSelector((state) => state.session.user);
+
     return (
-        <p>Reviews List</p>
+        <div>
+            {spotReviews?.map((review) => {
+                return (
+                    <div>
+                        <div>Review:{review.review}</div>
+                        <div>Rating:{review.rating} ⭐</div>
+                        <div>Created by:{sessionUser.username}</div>
+                    </div>
+                )
+            })}
+        </div>
     )
 }
