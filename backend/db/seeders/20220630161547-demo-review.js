a12d9087f70309ee5529cfb132a8242bc0ba0180
Jesse <jnjoroge13@gmail.com>
2022-07-04 16:35:58 -0700

    review seeding

--- backend/db/seeders/20220630161547-demo-review.js
+++ backend/db/seeders/20220630161547-demo-review.js
@@ -9,10 +9,34 @@ module.exports = {
         rating: 5,
 
       }, {
+        userId: 3,
+        spotId: 2,
+        review: 'Beautiful place.',
+        rating: 4,
+
+      },{
         userId: 2,
         spotId: 1,
         review: 'Outdated needs a remodel.',
         rating: 1,
+
+      }, {
+        userId: 3,
+        spotId: 1,
+        review: 'Just okay.',
+        rating: 3,
+
+      }, {
+        userId: 1,
+        spotId: 3,
+        review: 'Great Stay.',
+        rating: 5,
+
+      }, {
+        userId: 2,
+        spotId: 3,
+        review: 'Will be back.',
+        rating: 4,
 
       }], {});
   },
